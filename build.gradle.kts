535fde63a013274a5688d2a10b25a02cec97ee27
Daud Ibrahim Hasan <daudibrahimhasan@gmail.com>
2026-01-29 04:08:06 +0600

    i am still brainstorming

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "9.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
-    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
